709c3ab77cd8b2164d61de4a3a71925e2a94909d
M. Thirumala Rao <20131a05e5@gmail.com>
2023-06-14 01:08:34 +0000

    Image Display Successfull; Model Linking Trials

--- dynamic.py
+++ dynamic.py
@@ -2,22 +2,34 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.templating import Jinja2Templates
 import base64
+# import tensorflow
+# from tensorflow import keras
+from PIL import Image
+import matplotlib.pyplot as plt
 
-app = FastAPI()
+# app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+# templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def dynamic_file(request: Request):
-    return templates.TemplateResponse("dynamic.html", {"request": request})
+# @app.get("/")
+# async def dynamic_file(request: Request):
+#     return templates.TemplateResponse("dynamic.html", {"request": request})
 
-@app.post("/dynamic")
-def dynamic(request: Request, file: UploadFile = File()):
-    data = file.file.read()
-    file.file.close()
+# @app.post("/dynamic")
+# async def dynamic(request: Request, file: UploadFile = File()):
+#     data = file.file.read()
+#     file.file.close()
 
-    # encoding the image
-    encoded_image = base64.b64encode(data).decode("utf-8")
+#     # encoding the image
+#     encoded_image = base64.b64encode(data).decode("utf-8")
 
-    return templates.TemplateResponse(
-        "dynamic.html", {"request": request,  "img": encoded_image})
+#     #Demo Model Linking
+#     probability = model.predict(img2Array(image))
+
+#     return templates.TemplateResponse("dynamic.html", {"request": request,  "img": encoded_image, "prediction": probability})
+
+# # if __name__ == '__dynamic__':
+# #    uvicorn.run(app, host='0.0.0.0', port=8000)
+
+demo = Image.open("/workspace/Pheonix_Squadron/PDR-OS-LRG.jpg")
+plt.imshow(demo)
